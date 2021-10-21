ea5cc50334b99e8bfcd116ac4c180069f6cb54f7
Anna W <tester.kodilla@gmail.com>
2021-10-21 10:05:45 +0100

    added more tests

--- src/roman_numerals.test.js
+++ src/roman_numerals.test.js
@@ -32,20 +32,6 @@ test("throws an error if input not an Integer", () => {
     }).toThrow("Number must be an Integer");
 });
 
-// 1 - 3 I - III
-// 4 - 8 IV - VIII
-// 9 - 13 IX - XIII
-// 14 - 18 XIV - XVIII
-// 10 - 30 X - XXX
-// 40 - 80 XL - LXXX
-// 90 - 130 XC - CXXX
-// 140 -180 CXL - CLXXX
-// 100 - 300 C - CCC
-// 400 - 800 CD - DCCC
-// 900 - 1300 CM - MCCC
-// 1400 - 1800 MCD - MDCCC
-// 1000 - 3000 M - MMM
-
 test("returns I - III if input 1 - 3", () => {
     expect(getRomanNumeral(1)).toBe("I");
     expect(getRomanNumeral(3)).toBe("III");
@@ -59,5 +45,78 @@ test("returns IV - VIII if input 4 - 8", () => {
 
 test("returns IX - XIII if input 9 - 13", () => {
     expect(getRomanNumeral(9)).toBe("IX");
+    expect(getRomanNumeral(11)).toBe("XI");
     expect(getRomanNumeral(13)).toBe("XIII");
+});
+
+test("returns XIV - XVIII if input 14 - 18", () => {
+    expect(getRomanNumeral(14)).toBe("XIV");
+    expect(getRomanNumeral(15)).toBe("XV");
+    expect(getRomanNumeral(18)).toBe("XVIII");
+});
+
+test("returns X, XX or XXX if input 10, 20 or 30", () => {
+    expect(getRomanNumeral(10)).toBe("X");
+    expect(getRomanNumeral(20)).toBe("XX");
+    expect(getRomanNumeral(25)).toBe("XXV");
+});
+
+test("returns XL - LXXX if input 40 - 80", () => {
+    expect(getRomanNumeral(40)).toBe("XL");
+    expect(getRomanNumeral(70)).toBe("LXX");
+    expect(getRomanNumeral(80)).toBe("LXXX");
+});
+
+test("returns XC - CXXX if input 90 - 130", () => {
+    expect(getRomanNumeral(90)).toBe("XC");
+    expect(getRomanNumeral(110)).toBe("CX");
+    expect(getRomanNumeral(130)).toBe("CXXX");
+});
+
+test("returns CXL - CLXXX if input 140 - 180", () => {
+    expect(getRomanNumeral(140)).toBe("CXL");
+    expect(getRomanNumeral(150)).toBe("CL");
+    expect(getRomanNumeral(180)).toBe("CLXXX");
+});
+
+test("returns C, CC or CCC if input 100, 200 or 300", () => {
+    expect(getRomanNumeral(140)).toBe("CXL");
+    expect(getRomanNumeral(150)).toBe("CL");
+    expect(getRomanNumeral(180)).toBe("CLXXX");
+});
+
+test("returns CD - DCCC if input 400 - 800", () => {
+    expect(getRomanNumeral(400)).toBe("CD");
+    expect(getRomanNumeral(700)).toBe("DCC");
+    expect(getRomanNumeral(800)).toBe("DCCC");
+});
+
+test("returns CM - MCCC if input 900 - 1300", () => {
+    expect(getRomanNumeral(900)).toBe("CM");
+    expect(getRomanNumeral(1200)).toBe("MCC");
+    expect(getRomanNumeral(1300)).toBe("MCCC");
+});
+
+test("returns MCD - MDCCC if input 1400 - 1800", () => {
+    expect(getRomanNumeral(1400)).toBe("MCD");
+    expect(getRomanNumeral(1600)).toBe("MDC");
+    expect(getRomanNumeral(1800)).toBe("MDCCC");
+});
+
+test("returns M, MM or MMM if input 1000, 2000 or 3000", () => {
+    expect(getRomanNumeral(1000)).toBe("M");
+    expect(getRomanNumeral(2000)).toBe("MM");
+    expect(getRomanNumeral(3000)).toBe("MMM");
+});
+
+test("returns more complicated numerals correctly", () => {
+    expect(getRomanNumeral(34)).toBe("XXXIV");
+    expect(getRomanNumeral(53)).toBe("LIII");
+    expect(getRomanNumeral(27)).toBe("XXVII");
+    expect(getRomanNumeral(89)).toBe("LXXXIX");
+    expect(getRomanNumeral(99)).toBe("XCIX");
+    expect(getRomanNumeral(377)).toBe("CCCLXXVII");
+    expect(getRomanNumeral(531)).toBe("DXXXI");
+    expect(getRomanNumeral(1349)).toBe("MCCCXLIX");
+    expect(getRomanNumeral(2999)).toBe("MMCMXCIX");
 });
